666e8fa1599ceca952a07acf398259dd5ef52f68
Sameer Salman <titansam579@gmail.com>
2026-03-06 20:40:19 +0400

    Added Returns calculation and created targets

--- Main.py
+++ Main.py
@@ -12,4 +12,13 @@ def download_data(ticker="NVDA", start="2010-01-01"):
 df = download_data()
 print(df.head())
 
+#calculate returns
+df['Return'] = df['Adj Close'].pct_change()
+df = df.dropna()
 
+#create target
+df['Target'] = (df['Return'].shift(-1) > 0).astype(int)
+df = df.dropna()
+
+df["MA_5"] = df["Adj Close"].rolling(5).mean()
+df("MA_20") = df["Adj Close"].rolling(20).mean()
