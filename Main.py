d2c0ffd6eb1d790129360cc2a0f05e0f9908f53f
Sameer Salman <titansam579@gmail.com>
2026-03-07 15:22:30 +0400

    Added Logistic Regression

--- Main.py
+++ Main.py
@@ -3,6 +3,9 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression 
+from sklearn.preprocessing import StandardScaler 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 #download raw data
 def download_data(ticker="NVDA", start="2010-01-01"):
@@ -28,3 +31,31 @@ df["Momentum_5"] = df["Adj Close"] - df["Adj Close"].shift(5)
 df["Volatility_5"] = df["Return"].rolling(5).std()
 df["Volume_Change"] = df["Volume"].pct_change()
 
+#train/test split
+split_in = int(len(df)*0.8)
+train = df.iloc[split_in]
+test = df.iloc[split_in:]
+
+#baseline logistic regression
+features = ["MA_5", "MA_20", "Momentum_5", "Volatility_5", "Volume_Change"]
+X_train = train[features]
+y_train = train["Target"]
+
+X_train_scaled = StandardScaler.fit_transform(X_train)
+model = LogisticRegression(max_iter=2000)
+model.fit(X_train_scaled, y_train)
+
+#evaluate logistic regression
+X_test = test[features]
+y_test = test["Target"]
+X_test_scaled = StandardScaler.fit_transform(X_test)
+
+predictions = model.predict(X_test_scaled)
+
+print("Accuracy:", accuracy_score(y_test, predictions))
+print("Precision:", precision_score(y_test, predictions))
+print("Recall:", recall_score(y_test, predictions))
+print("F1 Score:", f1_score(y_test, predictions))
+print("Confusion MAtrix:\n", confusion_matrix(y_test, predictions))
+
+ 
